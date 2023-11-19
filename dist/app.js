d74de2fdf458869420f909c1988a291aae51d8f8
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-11-19 18:01:14 +0600

    #

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,34 @@ const port = 3000;
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+// create user router
+const userRouter = express_1.default.Router();
+// create course router
+const courseRouter = express_1.default.Router();
+// use user router
+app.use("/api/v1/users", userRouter);
+// use course router
+app.use("/api/v1/courses", courseRouter);
+//
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
+});
+// middleware
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course is created successfully",
+        data: course,
+    });
+});
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,39 @@ const port = 3000;
 app.use(express.json());
 app.use(express.text());
 
+// create user router
+const userRouter = express.Router();
+
+// create course router
+const courseRouter = express.Router();
+
+// use user router
+app.use("/api/v1/users", userRouter);
+
+// use course router
+app.use("/api/v1/courses", courseRouter);
+//
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
+});
+// middleware
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "Course is created successfully",
+    data: course,
+  });
+});
+
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
   next();
