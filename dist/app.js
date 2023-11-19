aa90428d4db361e7e6d17034e79fec1636e80c71
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-11-19 17:38:55 +0600

    learn about middleware

--- dist/app.js
+++ dist/app.js
@@ -9,12 +9,16 @@ const port = 3000;
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get("/", (req, res) => {
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get("/", logger, (req, res) => {
     console.log(req.params); // paramitter hishebe ja dibo ta aikhane pabo
     console.log(req.query); // query er jonno ja dibo ta aikhane pabo
     res.send("Hello world!");
 });
-app.post("/", (req, res) => {
+app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
         message: "Successfully received data ",
